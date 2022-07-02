f587bbbb5b1dfe89e879cb0e6670c9b37676d8ff
Erin Lee <erin.lee@coinbase.com>
2022-07-02 17:41:17 -0400

    Get data

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
+import * as dotenv from "dotenv";
 import puppeteer from "puppeteer";
+
+dotenv.config();
+
+const addressTrunc = `${process.env.ADDRESS}`.substring(0, 8);
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -20,11 +25,80 @@ import puppeteer from "puppeteer";
     }
   });
   await page.goto(`https://etherscan.io/address/${process.env.ADDRESS}`);
-  await page.screenshot({ path: `screenshots/full-${process.env.ADDRESS}.png`, fullPage: true });
 
   // Wait for the txns page to load and display the txns.
-  const txnsSelector = "a.hash-tag.myFnExpandBox_searchVal";
-  await page.waitForSelector(txnsSelector);
+  const txnsList = await page.$$eval(".hash-tag.myFnExpandBox_searchVal", (rows) =>
+    rows.map((el) => el.textContent)
+  );
+
+  console.log(txnsList.join("\n"));
+
+  const spentAmtTxnsList = [];
+
+  const currentYearOnly = new Date().getFullYear();
+  let isNotCurrentYear = false;
+
+  for (const txn of txnsList) {
+    await page.goto(`https://etherscan.io/tx/${txn}`, { waitUntil: "domcontentloaded" });
+    console.log("Transaction number: ", txn);
+
+    // Date of txn
+    const date = await page.$eval("#ContentPlaceHolder1_divTimeStamp", (ele) => {
+      const spanTextContent = ele.textContent?.match(/(?<=\().*(?=\))/) || [];
+      const dateString = spanTextContent[0].split(" ")[0];
+      const date = new Date(dateString);
+
+      return {
+        year: date.getFullYear(),
+        local: date.toLocaleDateString(),
+      };
+    });
+
+    isNotCurrentYear = date.year !== currentYearOnly;
+    if (isNotCurrentYear) {
+      console.log(`Exclude ${date.year} Transaction  ...`);
+    }
+    console.log("Date of txn: ", date);
+
+    const txnEthFee = await page.$eval("#ContentPlaceHolder1_spanTxFee", (ele) =>
+      ele.textContent?.split(" ")[0].replace(/[^0-9.]/g, "")
+    );
+    console.log("Transaction Fee (eth): ", txnEthFee);
+
+    const gasEthPrice = await page.$eval("#ContentPlaceHolder1_spanGasPrice", (ele) =>
+      ele.textContent?.split(" ")[0].replace(/[^0-9.]/g, "")
+    );
+    console.log("Gas Price (eth): ", gasEthPrice);
+
+    // Closing price of Ether on day of txn
+    const ethUSD = await page.$eval("#ContentPlaceHolder1_spanClosingPrice", (ele) =>
+      ele.textContent?.split(" ")[0].replace(/[^0-9.]/g, "")
+    );
+    console.log("Ether Closing USD: ", ethUSD);
+
+    const txnUsdFee = Number(txnEthFee) * Number(ethUSD);
+    const gasUsdFee = Number(gasEthPrice) * Number(ethUSD);
+    const spentAmt = txnUsdFee + gasUsdFee;
+
+    if (!isNotCurrentYear) {
+      spentAmtTxnsList.push(spentAmt);
+    }
+
+    console.log("Transaction Fee (USD): ", txnUsdFee);
+    console.log("Gas Fee (USD): ", gasUsdFee);
+    console.log("TOTAL: $", spentAmt);
+
+    const imageFile = `${addressTrunc}-${txn?.substring(0, 8)}`;
+    await page.screenshot({
+      path: `screenshots/${imageFile}.png`,
+      fullPage: true,
+    });
+
+    console.log("-----");
+  }
+
+  const totalSpentAmt = spentAmtTxnsList.reduce((prev, curr) => prev + curr, 0);
+  console.log("Total Spent Amt: ", totalSpentAmt);
 
   await browser.close();
 })();
